07f4a8cabbbebf4cf2e668c741262a3582036d7a
Paulo Honorato <paulohonoratos@hotmail.com>
2021-07-13 10:39:59 -0300

    Criação do endpoint Cadastrar Usuário e validação de email, pis e cpf

--- backend/src/main/java/com/paulohonorato/pontotel/colaborators/controllers/ColaboradorController.java
+++ backend/src/main/java/com/paulohonorato/pontotel/colaborators/controllers/ColaboradorController.java
@@ -3,13 +3,18 @@ package com.paulohonorato.pontotel.colaborators.controllers;
 import java.util.List;
 
 import com.paulohonorato.pontotel.colaborators.dtos.ColaboradorDTO;
+import com.paulohonorato.pontotel.colaborators.entities.Colaborador;
 import com.paulohonorato.pontotel.colaborators.services.ColaboradorService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping("/colaborators")
@@ -23,4 +28,29 @@ public class ColaboradorController {
         List<ColaboradorDTO> list = service.findAll();
         return ResponseEntity.ok(list);
     }
+
+    @PostMapping
+    public ResponseEntity cadastrar(@RequestBody ColaboradorDTO dto) {
+        Colaborador colaborador = new Colaborador();
+        colaborador.setNome(dto.getNome());
+        colaborador.setEmail(dto.getEmail());
+        colaborador.setPais(dto.getPais());
+        colaborador.setEstado(dto.getEstado());
+        colaborador.setMunicipio(dto.getMunicipio());
+        colaborador.setCep(dto.getCep());
+        colaborador.setRua(dto.getRua());
+        colaborador.setNumero(dto.getNumero());
+        colaborador.setComplemento(dto.getComplemento());
+        colaborador.setCpf(dto.getCpf());
+        colaborador.setPis(dto.getPis());
+        colaborador.setSenha(dto.getSenha());
+
+        try {
+            Colaborador colaboradorSalvo = service.cadastrar(colaborador);
+            return new ResponseEntity(colaboradorSalvo, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+    
 }
