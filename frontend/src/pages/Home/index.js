69a187aac669b7596cae91ab8bf592669d073619
Paulo Honorato <paulohonoratos@hotmail.com>
2021-07-14 12:52:32 -0300

    Criação do componente de atualização dos dados cadastrais

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,15 +1,22 @@
+import Card from 'components/Card'
 import React from 'react'
 
 class Home extends React.Component {
 
+    atualizar = () => {
+        this.props.history.push('/atualizar')
+    }
+
     logout = () => {
         this.props.history.push('/')
     }
+
     render(){
         return (
             <div>
-                <h1>Testando</h1>
-                <button className="btn btn-primary" onClick={this.logout}>Logout</button>
+                <h1>Olá Visitante!</h1>
+                <button className="btn btn-primary" onClick={this.atualizar}>Atualizar Dados</button>                
+                <button className="btn btn-danger" onClick={this.logout}>Logout</button>
             </div>
         )
     }
