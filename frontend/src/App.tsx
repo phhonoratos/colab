8f3c91ed5650e80385dc7aec1d0d7d804b8be98a
Paulo Honorato <paulohonoratos@hotmail.com>
2021-07-13 21:09:09 -0300

    Criação do componente Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,12 @@
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
+import Home from "pages/Home";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="container">
-        <h1>Olá mundo!</h1>
-      </div>
+      <Home />
       <Footer />
     </>
   );
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,11 @@
+function Home(){
+    return (
+        <div className="container">
+            <div className="home-content">
+                <h1>Olá Visitante!</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
