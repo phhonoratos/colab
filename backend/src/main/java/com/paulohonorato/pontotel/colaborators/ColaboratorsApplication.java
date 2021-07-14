b3a81b0b01e73f6cb2c27da3c275173116624da5
Paulo Honorato <paulohonoratos@hotmail.com>
2021-07-14 19:31:27 -0300

    Adicionando service de localStorage

--- backend/src/main/java/com/paulohonorato/pontotel/colaborators/ColaboratorsApplication.java
+++ backend/src/main/java/com/paulohonorato/pontotel/colaborators/ColaboratorsApplication.java
@@ -2,9 +2,18 @@ package com.paulohonorato.pontotel.colaborators;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ColaboratorsApplication {
+@EnableWebMvc
+public class ColaboratorsApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("*/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ColaboratorsApplication.class, args);
