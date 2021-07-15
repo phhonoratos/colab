1e1b4f35f23280bc316eff9a8827b89d88c691a8
Paulo Honorato <paulohonoratos@hotmail.com>
2021-07-14 23:33:32 -0300

    Adicionando dependencia do lombok e refatorando as classes do backend

--- backend/src/main/java/com/paulohonorato/pontotel/colaborators/entities/Colaborador.java
+++ backend/src/main/java/com/paulohonorato/pontotel/colaborators/entities/Colaborador.java
@@ -8,8 +8,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
 @Table(name = "tb_colaborators")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Colaborador implements Serializable {
     private static final long serialVerionUID = 1L;
 
@@ -29,162 +36,8 @@ public class Colaborador implements Serializable {
     private String pis;
     private String senha;
 
-    public Colaborador() {
-    }
-
-    public Colaborador(Long id, String nome, String email, String rua, String numero, String complemento, String cep,
-            String municipio, String estado, String pais, String cpf, String pis, String senha) {
-        this.id = id;
-        this.nome = nome;
-        this.email = email;
-        this.rua = rua;
-        this.numero = numero;
-        this.complemento = complemento;
-        this.cep = cep;
-        this.municipio = municipio;
-        this.estado = estado;
-        this.pais = pais;
-        this.cpf = cpf;
-        this.pis = pis;
-        this.senha = senha;
-    }
-
     public static long getSerialverionuid() {
         return serialVerionUID;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRua() {
-        return rua;
-    }
-
-    public void setRua(String rua) {
-        this.rua = rua;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getMunicipio() {
-        return municipio;
-    }
-
-    public void setMunicipio(String municipio) {
-        this.municipio = municipio;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getPais() {
-        return pais;
-    }
-
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getPis() {
-        return pis;
-    }
-
-    public void setPis(String pis) {
-        this.pis = pis;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Colaborador other = (Colaborador) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Colaborador [id=" + id + "]";
     }
 
 }
