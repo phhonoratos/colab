befcc069b64dec84181cf1e68a3a08faaacefe1e
Paulo Honorato <paulohonoratos@hotmail.com>
2021-07-16 15:47:56 -0300

    Estilização das páginas e consumo de api para cep

--- backend/src/main/java/com/paulohonorato/pontotel/colaborators/services/ColaboradorService.java
+++ backend/src/main/java/com/paulohonorato/pontotel/colaborators/services/ColaboradorService.java
@@ -58,37 +58,4 @@ public class ColaboradorService {
         return repository.findById(id);
     }
 
-    // public void validarCadastro(Colaborador colaborador) {
-    //     if(repository.existsByEmail(colaborador.getEmail())) {
-    //         throw new RegraDeNegocioException("Já existe um usuário cadastrado com este email.");
-    //     }
-    //     if(repository.existsByCpf(colaborador.getCpf())) {
-    //         throw new RegraDeNegocioException("Já existe um usuário cadastrado com este cpf.");
-    //     }
-    //     if(repository.existsByPis(colaborador.getPis())) {
-    //         throw new RegraDeNegocioException("Já existe um usuário cadastrado com este pis.");
-    //     }
-    // }
-
-    // public void ValidarAtualizacao(Colaborador colaborador) {
-    //     if(colaborador.getNome() == null || colaborador.getNome().trim().equals("")) {
-    //         throw new RegraDeNegocioException("Informe um NOME com pelo menos 4 letras.");
-    //     }
-    //     if(colaborador.getCep() == null || colaborador.getCep().trim().equals("")) {
-    //         throw new RegraDeNegocioException("Informe um CEP válido.");
-    //     }
-    //     if(colaborador.getNumero() == null || colaborador.getNumero().trim().equals("")) {
-    //         throw new RegraDeNegocioException("Informe um NÚMERO de endereço válido.");
-    //     }
-    //     if(colaborador.getCpf() == null || colaborador.getCpf().trim().equals("")) {
-    //         throw new RegraDeNegocioException("Informe um CPF válido.");
-    //     }
-    //     if(colaborador.getPis() == null || colaborador.getPis().trim().equals("")) {
-    //         throw new RegraDeNegocioException("Informe um PIS válido.");
-    //     }
-    //     if(colaborador.getSenha() == null || colaborador.getSenha().trim().equals("")) {
-    //         throw new RegraDeNegocioException("Informe uma SENHA.");
-    //     }
-    // }
-
 }
--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import Card from 'components/Card'
 import axios from 'axios'
 import './styles.css'
 import LocalStorageService from 'app/service/localStorageService'
@@ -9,7 +8,6 @@ class Login extends React.Component {
     state = {
         email: "",
         senha: "",
-        msgErro: null
     }
 
     entrar = () => {
@@ -21,7 +19,6 @@ class Login extends React.Component {
             this.props.history.push('/home')
         }).catch( erro => {
             alert('Email ou senha inválidos')
-            // this.setState({msgErro: erro.response.data})
             console.log(erro.response)
         })
 
@@ -34,13 +31,12 @@ class Login extends React.Component {
     render() {
         return (
             <div className="container">
-                <div className="home-content">
-                    <h3 className="mt-3">Olá Visitante!</h3>
-                    <Card title="Login" className="card">
-                        <div>
-                            <span>{this.state.msgErro}</span>
-                        </div>
-                    <fieldset>
+                <h3 className="mt-3">Olá Visitante!</h3>
+                <div className="card">
+                    <div className="card-header">
+                        <h5>Login</h5>
+                    </div>
+                    <div className="card-body">
                         <div className="form-group mt-4">
                             <input type="text" 
                                 value={this.state.email}
@@ -55,13 +51,12 @@ class Login extends React.Component {
                                 className="form-control" 
                                 placeholder="Password" />
                         </div>
-                    </fieldset>
-                    <div className="botoes">
-                        <button className="btn btn-success" onClick={this.entrar}>Entrar</button>
-                        <h5 className="mt-4">OU</h5>
-                        <button className="btn btn-danger" onClick={this.Cadastrar}>Cadastre-se</button>
+                        <div className="botoes">
+                            <button className="btn btn-success" onClick={this.entrar}>Entrar</button>
+                            <h5 className="mt-4">OU</h5>
+                            <button className="btn btn-danger" onClick={this.Cadastrar}>Cadastre-se</button>
+                        </div>
                     </div>
-                </Card>
                 </div>
             </div>
         );
